3309c779e2126c78367fcd1401426e35d18437c8
DarrellGehring <darrell@wasatch-solutions.com>
2022-08-24 15:17:33 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AlgoliaSearch/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlgoliaSearch from './components/AlgoliaSearch.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlgoliaSearch
   }
 }
 </script>
---
+++ src/components/AlgoliaSearch.vue
@@ -0,0 +1,35 @@
+<template>
+  <ais-instant-search :search-client="searchClient" index-name="podcasts">
+    <ais-search-box />
+    <ais-hits>
+      <template v-slot:item="{ item }">
+        <h2>{{ item.title }} published by {{ item.publisher }}</h2>
+      </template>
+    </ais-hits>
+    <ais-pagination />
+  </ais-instant-search>
+</template>
+
+<script>
+import algoliasearch from 'algoliasearch/lite';
+import 'instantsearch.css/themes/satellite-min.css';
+
+export default {
+    name: 'AlgoliaSearch',
+    data() {
+        return {
+            searchClient: algoliasearch(
+                '032WC3B6ZG',
+                '92af7d487c37a7a95bff764c5f8a5b16'
+            ),
+        };
+    },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  padding: 1em;
+}
+</style>
